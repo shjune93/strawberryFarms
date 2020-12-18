ba99b34a055e5f547f433af88dd1f0a724dde0eb
Huijun <shjune93@gmail.com>
2020-12-18 21:39:33 +0900

    django 8 add detail page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.filter(title__contains='title').order_by('-created_date')
+    posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
